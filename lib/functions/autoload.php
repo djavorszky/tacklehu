e2e5de129b037552b9f25381094daa6760849541
djavorszky <daniel.javorszky@liferay.com>
2016-03-11 10:10:11 +0100

    Move everything under lib

--- lib/functions/autoload.php
+++ lib/functions/autoload.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function ($name) {
     if (debug()) debug_echo("Loading class $name.");
-    require_once("classes/$name.php");
+    require_once("lib/classes/$name.php");
 });
 
 ?>
