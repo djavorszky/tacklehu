05a5f344c4049d405b2b2761300ef62672f225d1
djavorszky <daniel.javorszky@liferay.com>
2016-03-11 10:05:21 +0100

    Add material sources and include in index.php

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ require_once("config.php");
 
 <html>
 <head>
-	<title></title>
+	<title>tackle.hu</title>
+	<link rel="stylesheet" href="./res/material.min.css">
+	<script src="./res/material.min.js"></script>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
 	<h1>It's alive! :-)</h1>
