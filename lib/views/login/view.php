ca5bc302c010306636ba120315c13b7d734ab436
djavorszky <daniel.javorszky@liferay.com>
2016-03-14 19:20:58 +0100

    Bootstrapify login screen

--- lib/views/login/view.php
+++ lib/views/login/view.php
@@ -1,28 +1,30 @@
-<div class="mdl-grid">
-	<div class="mdl-cell mdl-cell--6-col mdl-cell--3-offset-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone">
-		<div class="mdl-cell mdl-cell--12-col">
-			<div class="mdl-typography mdl-typography--headline">Sign in</div>
-		</div>
-		<form action="<?php echo Config::getURL()?>/login" method="POST">
-			<input type="hidden" name="action" value="doLogin">
-			<div class="mdl-cell mdl-cell--5-col mdl-cell--10-col-tablet mdl-cell--4-col-phone mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-				<input class="mdl-textfield__input" type="email" name="email" id="email">
-				<label class="mdl-textfield__label" for="email">Email</label>
-				<span class="mdl-textfield__error">Something's wrong</span>
-			</div>
-			<div class="mdl-cell mdl-cell--5-col mdl-cell--10-col-tablet mdl-cell--4-col-phone mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-				<input class="mdl-textfield__input" type="password" name="password" id="password">
-				<label class="mdl-textfield__label" for="password">Password</label>
-				<span class="mdl-textfield__error">Field is required.</span>
-			</div>
-			<div class="mdl-cell mdl-cell-12-col">
-				<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored">
-				  Sign in
-				</button>
-			</div>
-			<div class="mdl-cell mdl-cell--10-col">
-				<a href="<?php echo Config::getURL()?>/register">Don't have an account yet? Click here to register.</a>
-			</div>
-		</form>
+<div class="row">
+	<div class="page-header col-sm-5 col-sm-offset-3">
+		<h1>Sign in</h1>
 	</div>
 </div>
+<form class="form-horizontal" action="<?php echo Config::getURL()?>/login" method="POST">
+	<input type="hidden" name="action" value="doLogin">
+	<div class="form-group">
+		<label for="email" class="col-sm-2 col-sm-offset-2 control-label">Email</label>
+		<div class="col-sm-4">
+			<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="password" class="col-sm-2 col-sm-offset-2 control-label">Password</label>
+		<div class="col-sm-4">
+			<input type="password" class="form-control" id="password" name="password" placeholder="Password">
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-offset-4 col-sm-10">
+			<button type="submit" class="btn btn-default">Sign in</button>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-offset-4">
+			<a href="<?php echo Config::getURL()?>/register">Don't have an account yet? Click here to register.</a>
+		</div>
+	</div>
+</form>
