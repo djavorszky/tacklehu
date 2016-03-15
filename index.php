d58340c0321f49687041581748ac99c3977bc7a9
djavorszky <daniel.javorszky@liferay.com>
2016-03-15 13:00:21 +0100

    Add meta tag to fix mobile views

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <?php
 require_once("lib/init.php");
 
@@ -10,6 +10,7 @@ $response = Handler::respond($_GET, $_POST);
 	<title><?php echo R::lang("site-title-key") ?></title>
 	<?php Config::printBootstrapAndJQueryResources() ?>
 	<link rel="shortcut icon" href="<?php echo Config::getURL()?>/res/black-letter-t.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	<?php require_once("lib/views/common/header.php"); ?>
