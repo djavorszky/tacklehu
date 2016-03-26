773d3bc9c774edcc9af846c5dfbdfc2fcc36b6a5
djavorszky <daniel.javorszky@liferay.com>
2016-03-26 17:16:23 +0100

    Langify sidebar

--- lib/views/common/sidebar.php
+++ lib/views/common/sidebar.php
@@ -1,4 +1,4 @@
 <div class="col-sm-4">
-	<h2>Placeholder kitten</h2>
+	<h2><?php echo R::lang("placeholder-cat") ?></h2>
 	<img src="<?php echo Config::getURL("/res/img/placeholdercat.jpg") ?>" alt="coming soon..">
 </div>
